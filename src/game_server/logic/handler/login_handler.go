d4e9e297218fbd45476e1c40a9d2ade9bffee421
cyinx <1494193739@qq.com>
2018-04-12 15:27:10 +0800

    fix some bugs

--- src/game_server/logic/handler/login_handler.go
+++ src/game_server/logic/handler/login_handler.go
@@ -3,6 +3,7 @@ package msghandler
 import (
 	//"game_server/logic/client_mgr"
 	"game_server/logic/module"
+	"github.com/Cyinx/einx/lua"
 	"github.com/Cyinx/einx/slog"
 	"msg_def"
 )
@@ -25,6 +26,6 @@ func CheckVersion(agent Agent, args interface{}) {
 func MsgLuaRpc(agent Agent, args interface{}) {
 	msg := args.(*msg_def.LuaRpcMsg)
 	lua_runtime := module.Lua
-	lua_arg, _ := lua_runtime.UnMarshal(msg.Payload)
+	lua_arg, _ := lua_state.UnMarshal(msg.Payload, lua_runtime.GetVm())
 	lua_runtime.PCall("on_message_handler", msg.Rpcname, uint64(agent.GetID()), lua_arg)
 }
